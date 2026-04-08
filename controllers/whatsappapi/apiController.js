9cc7898c375b8eab091a6d654c1018d77019ffab
digitalkstechno <digitalkstechno@gmail.com>
2026-04-08 11:49:41 +0530

    fix

--- controllers/whatsappapi/apiController.js
+++ controllers/whatsappapi/apiController.js
@@ -8,7 +8,27 @@ exports.getContactByDataFetch = async (req, res) => {
         if (!client) {
             return res.status(404).json({ message: 'Client not found' });
         }
-        res.json(client);
+        let dataRecord = null;
+        if (req.params.person === 'Self') {
+            dataRecord = { name: client.name, documents: client.documents };
+        } else {
+            const person = client.familyMembers.find(member => member.name === req.params.person);
+            dataRecord = person ? { name: person.name, documents: person.documents } : null;
+        }
+        let document = null;
+        if (req.params.doc != 'ITR' && req.params.doc != 'Other') {
+            let data = dataRecord.documents.find(doc => doc.category === req.params.doc);
+            document = data ? data : null;
+        } else {
+            if (req.params.doc === 'ITR') {
+                let data = dataRecord.documents.find(doc => doc.category === req.params.doc && doc.itrYear === req.query.year);
+                document = data ? data : null;
+            } else if (req.params.doc === 'Other') {
+                let data = dataRecord.documents.find(doc => doc.category === req.params.doc && doc.subCategory === req.query.cat);
+                document = data ? data : null;
+            }
+        }
+        res.json({ subCategory: document.subCategory, category: document.category, filePath: process.env.backend_URL + document.filePath });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
@@ -16,12 +36,12 @@ exports.getContactByDataFetch = async (req, res) => {
 
 exports.getContactByFamilyDataFetch = async (req, res) => {
     try {
-        const client = await Client.findOne({ isDeleted: false, phone: req.params.phone, serviceEnabled: true });
+        const client = await Client.findOne({ isDeleted: false, phone: req.params.phone });
         if (!client) {
             return res.status(404).json({ message: 'Client not found' });
         }
         let Data = client.familyMembers.map(member => member.name);
-        res.json(Data);
+        res.json(['Self', ...Data]);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
--- routes/whatsapp/api.js
+++ routes/whatsapp/api.js
@@ -5,6 +5,6 @@ const {
     getContactByFamilyDataFetch,
 } = require('../../controllers/whatsappapi/apiController');
 
-router.get('/:phone', getContactByDataFetch);
 router.get('/family/:phone', getContactByFamilyDataFetch);
+router.get('/:phone/:person/:doc', getContactByDataFetch);
 module.exports = router;
