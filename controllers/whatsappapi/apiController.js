735ce1d3d576a76fc20c1d12f83a979a65f3bf26
digitalkstechno <digitalkstechno@gmail.com>
2026-04-13 16:01:31 +0530

    fix: update response format in getContactByFamilyDataFetch to return names as strings

--- controllers/whatsappapi/apiController.js
+++ controllers/whatsappapi/apiController.js
@@ -41,7 +41,7 @@ exports.getContactByFamilyDataFetch = async (req, res) => {
             return res.status(404).json({ message: 'Client not found' });
         }
         let Data = client.familyMembers.map(member => member.name);
-        res.json([{name: 'Self'}, ...Data.map(name => ({name}))]);
+        res.json(['Self', ...Data]);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
